693702d35188c0aa4dc31353d64f8b8dd788d04f
KiHwan Yun <drg2524@naver.com>
2024-01-27 17:13:00 +0900

    Servlet File Upload Section commit

--- src/main/java/hello/upload/controller/ServletUploadControllerV2.java
+++ src/main/java/hello/upload/controller/ServletUploadControllerV2.java
@@ -16,7 +16,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
-
+/*
+*   [Servlet File Upload Two]
+*
+*   서블릿이 제공하는 Part에 대해 알아보고 실제 파일도 서버에 업로드 해보자.
+*
+*   먼저 파일을 업로드를 하려면 실제 파일이 저장되는 경로가 필요하다.
+*
+*   해당 경로에 실제 폴더를 만들어두자.
+*   그리고 다음에 만들어진 경로를 입력해두자.
+*
+*   [application.properties]
+*   file.dir=파일 업로드 경로 설정(예): /Users/yunkihwan/study/file/
+*
+*   <주의>
+    꼭 해당 경로에 실제 폴더를 미리 만들어두자.
+    application.properties에서 설정할 때 마지막에 /(슬래시)가 포함된 것에 주의하자.
+* */
 @Slf4j
 @Controller
 @RequestMapping("/servlet/v2")
@@ -68,8 +84,49 @@ public class ServletUploadControllerV2 {
         return "upload-form";
     }
 //    application.properties 에서 설정한 file.dir 의 값을 주입한다.
-    
+
 //    멀티파트 형식은 전송 데이터를 하나하나 각각 부분( Part )으로 나누어 전송한다. parts 에는 이렇게
 //    나누어진 데이터가 각각 담긴다.
 //    서블릿이 제공하는 Part 는 멀티파트 형식을 편리하게 읽을 수 있는 다양한 메서드를 제공한다
+
+    /*
+    *   [Part 주요 메서드]
+    *
+    *   part.getSubmittedFileName() : 클라이언트가 전달한 파일명
+    *   part.getInputStream() : Part의 전송 데이터를 읽을 수 있다.
+    *   part.write(...) : Part를 통해 전송된 데이터를 저장할 수 있다.
+    *
+    *   [결과 로그]
+    *
+    *   ==== PART ====
+        name=itemName
+        header content-disposition: form-data; name="itemName"
+        submittedFileName=null
+        size=7
+        body=상품A
+        ==== PART ====
+        name=file
+        header content-disposition: form-data; name="file"; filename="스크린샷.png"
+        header content-type: image/png
+        submittedFileName=스크린샷.png
+        size=112384
+        body=qwlkjek2ljlese...
+        파일 저장 fullPath=/Users/kimyounghan/study/file/스크린샷.png
+    *
+    *  =======================================================================
+    *  파일 저장 경로에 가보면 실제 파일이 저장된 것을 확인할 수 있다.
+    *  만약 저장이 되지 않았다면 파일 저장 경로를 다시 확인하자.
+    *
+    *  [참고]
+    *  큰 용량의 파일을 업로드를 테스트 할 때는 로그가 너무 많이 남아서 다음 옵션을 끄는 것이 좋다.
+    *  logging.level.org.apache.coyote.http11=debug
+    *  다음 부분도 파일의 바이너리 데이터를 모두 출력하므로 끄는 것이 좋다.
+    *  log.info("body = {}" , body);
+    *
+    *  서블릿이 제공하는 Part는 편하기는 하지만, HttpServletRequest를 사용해야 하고,
+    *  추가로 파일 부분만 구분하려면 여러가지 코드를 넣어야 한다.
+    *  이번에는 스프링이 이 부분을 얼마나 편리하게 제공하는지 확인해보자.
+    * 
+    *
+    * */
 }

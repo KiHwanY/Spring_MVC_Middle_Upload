db56233c959bd50eb5b36f91731ee131a53d1b78
KiHwan Yun <drg2524@naver.com>
2024-01-29 00:06:00 +0900

    File Upload Section commit

--- src/main/java/hello/upload/domain/UploadFile.java
+++ src/main/java/hello/upload/domain/UploadFile.java
@@ -5,6 +5,9 @@ import lombok.Data;
 @Data
 public class UploadFile {
 
+//    uploadFileName : 고객이 업로드한 파일명
+//    storeFileName : 서버 내부에서 관리하는 파일명
+
     private String uploadFileName;
     private String storeFileName;
 
@@ -12,4 +15,12 @@ public class UploadFile {
         this.uploadFileName = uploadFileName;
         this.storeFileName = storeFileName;
     }
+
+    /*
+    *   고객이 업로드한 파일명으로 서버 내부에 파일을 저장하면 안된다.
+    *   왜냐하면 서로 다른 고객이 같은 파일 이름을 업로드 하는 경우 기존 파일 이름과 충돌이 날 수 있다.
+    *   서버에서는 저장할 파일명이 겹치지 않도록 내부에서 관리하는 별도의 파일명이 필요하다.
+    * */
+
+
 }
